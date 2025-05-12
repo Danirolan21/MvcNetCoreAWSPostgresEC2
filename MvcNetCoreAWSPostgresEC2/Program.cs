de9c9bd82d364be5aae2f6896333f4b754a6077d
Danirolan21 <danirolan21@hotmail.com>
2025-05-12 13:30:26 +0200

    commit

--- MvcNetCoreAWSPostgresEC2/Program.cs
+++ MvcNetCoreAWSPostgresEC2/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddTransient<RepositoryHospitales>();
 builder.Services.AddDbContext<HospitalContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+    options.UseMySQL(builder.Configuration.GetConnectionString("Mariadb")));
 
 builder.Services.AddControllersWithViews();
 
